bdbaf9a6f20b08ca8ec6851e04b1c04d03229ebb
salfra <salfra@email.unc.edu>
2023-03-30 16:51:41 -0400

    res.end(data_var) instead of just data

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(data);
+  res.end(data_var);
 });
 
 server.listen(port);
